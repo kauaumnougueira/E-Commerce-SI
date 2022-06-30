8f5fce1d549dcabebf067afbba75ea7630b0c3d6
João Pedro Ruas <83050781+hagsir@users.noreply.github.com>
2022-06-29 22:15:14 -0300

    Update main 29.06.c

--- main 29.06.c
+++ main 29.06.c
@@ -11,11 +11,11 @@ BUILD:
     2- INTERFACE PARA CADASTRO
     3- INTERFACE DE LOGIN
         3.1- INTERFACE DE CLIENTES
-        3.2- INTERFACE DE FUNCION¡RIOS
+        3.2- INTERFACE DE FUNCION√ÅRIOS
     #FUNCTIONS
 */
 
-//OP«√O: ADCIONAR STRUCT PARA CATEGORIA (FUNCION¡RIO OU CLIENTE)
+//OP√á√ÉO: ADCIONAR STRUCT PARA CATEGORIA (FUNCION√ÅRIO OU CLIENTE)
 
 //DATA
 struct User{
@@ -28,9 +28,10 @@ struct Product{
     char nameProd[50];
     char description[250];
     int quantity;
+    float preco;
 };
 
-//CHAMANDO FUN«’ES
+//CHAMANDO FUN√á√ïES
 void cliente();
 void vendedor();
 void login(struct User userAtual, struct User users[20], int index);
@@ -40,7 +41,7 @@ void cadastro(struct User userAtual, struct User users[20], int index);
 void voltar();
 
 main(){
-    //CONFIGURA«’ES DO SISTEMA
+    //CONFIGURA√á√ïES DO SISTEMA
     setlocale(LC_ALL, "Portuguese");
     system("color A");
 
@@ -52,29 +53,31 @@ main(){
 
 //INTERFACE INICIAL
 void inicial(int index){
-    system("cls");
 
-    //INST¬NCIA DO USU¡RIO
+
+    //INST√ÇNCIA DO USU√ÅRIO
     struct User userAtual;
     struct User users[20];
 
     int count = 0, cadLog;
 
     while(count == 0){
-        printf("___Seja bem-vindo ‡ loja virtual!___");
+        printf("___Seja bem-vindo √† loja virtual!___");
         printf("\n|                                  |");
-        printf("\n|   VocÍ deseja:                   |\n|   ( 1 ) Fazer Login              |\n|   ( 2 ) Fazer Cadastro           |\n|__________________________________|\n> ");
+        printf("\n|   Voc√™ deseja:                   |\n|   ( 1 ) Fazer Login              |\n|   ( 2 ) Fazer Cadastro           |\n|__________________________________|\n> ");
         scanf("%d", &cadLog);
         getchar(); //Soluciona erro de tipagem do input
         if(cadLog == 2){
             count++;
+            system("cls");
             cadastro(userAtual, users, index);
         }else if(cadLog == 1){
             count++;
+            system("cls");
             login(userAtual, users, index);
         }else{
             system("cls");
-            error("N„o identificado, selecione somente os n˙meros ao lado das opÁıes\n");
+            error("N√£o identificado, selecione somente os n√∫meros ao lado das op√ß√µes\n");
         }
     }
 }
@@ -94,13 +97,16 @@ void cadastro(struct User userAtual, struct User users[20], int index){
         }
     }
     if(existe == 1){
-        printf("Usu·rio j· cadastrado\n Sendo redirecionado para LOGIN...");
+        printf("Usu√°rio j√° cadastrado\n Sendo redirecionado para LOGIN...");
         login(userAtual, users, index);
     }else{
         int count = 0;
         int categoria;
         while(count == 0){
-            printf("|   VocÍ deseja se cadastrar como cliente ou vendedor?  |\n( 1 ) Cliente\n( 2 ) Vendedor\n( 3 ) Voltar para o menu inicial\n> ");
+            printf("|                                                       |");
+            printf("\n|   Voc√™ deseja se cadastrar como cliente ou vendedor?  |");
+            printf("\n|   ( 1 ) Cliente                                       |\n|   ( 2 ) Vendedor                                      |\n|   ( 3 ) Voltar para o menu inicial                    |");
+            printf("\n|_______________________________________________________|\n>");
             scanf("%d", &categoria);
             getchar(); //Soluciona erro de tipagem do input
             //SALVANDO CATEGORIA
@@ -118,7 +124,7 @@ void cadastro(struct User userAtual, struct User users[20], int index){
                 count++;
             }else{
                 system("cls");
-                error("Categoria n„o identificada");
+                error("Categoria n√£o identificada");
             }
         }
         count = 0;
@@ -131,19 +137,20 @@ void cadastro(struct User userAtual, struct User users[20], int index){
         getchar();
         fgets(userAtual.name, 50, stdin);
          while(count == 0){
-            printf("Digite sua senha (no m·ximo 10 caracteres): ");
+            printf("Digite sua senha (no m√°ximo 10 caracteres): ");
             fgets(senha1, 10, stdin);
             printf("Confirme sua senha: ");
             fgets(senha2, 10, stdin);
             if(strcmp(senha1, senha2) < 0 || strcmp(senha1, senha2) > 0){
                 system("cls");
-                error("Senhas n„o correspondentes");
+                error("Senhas n√£o correspondentes");
                 count=0;
             }else{
-                printf("Cadastrado!");
+                system("cls");
+                printf("Usu√°rio cadastrado com sucesso!");
                 count++;
                 strcpy(userAtual.senha, senha1);
-                login(userAtual, users, index); //USU¡RIO CADASTRADO
+                login(userAtual, users, index); //USU√ÅRIO CADASTRADO
                 salvaStruct(userAtual, users, index);
             }
         }
@@ -152,14 +159,14 @@ void cadastro(struct User userAtual, struct User users[20], int index){
 
 //INTERFACE DE LOGIN
 void login(struct User userAtual, struct User users[20], int index){
-    system("cls");
-    printf("______________LOGIN________________");
+
+    printf       ("_________________LOGIN___________________");
     int count = 0;
     int categoria;
     while(count==0){
-        printf("\n|                                 |");
-        printf("\n|    Informe a sua categoria:     |\n|    ( 1 ) Cliente                |\n|    ( 2 ) Vendedor               |");
-        printf("\n|_________________________________|\n>");
+        printf("\n|                                       |");
+        printf("\n|    Informe a sua categoria:           |\n|    ( 1 ) Cliente                      |\n|    ( 2 ) Vendedor                     |\n|    ( 3 ) Voltar para o menu inicial   |");
+        printf("\n|_______________________________________|\n>");
         scanf("%d", &categoria);
         getchar(); //Soluciona erro de tipagem do input
         //SALVANDO CATEGORIA
@@ -169,9 +176,13 @@ void login(struct User userAtual, struct User users[20], int index){
         }else if(categoria == 2){
             strcpy(userAtual.category, "vendedor");
             count++;
+        }else if(categoria == 3){
+            system("cls");
+            voltar();
+            count++;
         }else{
             system("cls");
-            error("Categoria n„o identificada");
+            error("Categoria n√£o identificada");
        }
     }
     //LOGIN
@@ -186,14 +197,14 @@ void login(struct User userAtual, struct User users[20], int index){
         fgets(name, 50, stdin);
         if(strcmp(name, userAtual.name) < 0 || strcmp(name, userAtual.name) > 0){
             system("cls");
-            error("Nome n„o cadastrado...");
-            printf("Deseja cadastrar? \n( 1 ) Sim \n( 2 ) N„o\n> ");
+            error("Nome n√£o cadastrado...");
+            printf("Deseja cadastrar? \n( 1 ) Sim \n( 2 ) N√£o\n> ");
             scanf("%d", &escolha);
             if(escolha == 1){
                 cadastro(userAtual, users, index);
             }else{
                 count=0;
-                voltar(); //SE A PESSOA ESCOLHER N√O SE CADATRAR O PROGRAMA TEM QUE VOLTAR PRA TELA INICIALL, COMO FAZ ISSO?????????????????
+                voltar();
                 break;
             }
             count++;
@@ -201,12 +212,14 @@ void login(struct User userAtual, struct User users[20], int index){
             printf("Digite sua senha: ");
             fgets(senha, 10, stdin);
             if(strcmp(senha, userAtual.senha) < 0 || strcmp(senha, userAtual.senha) > 0){
+                system("cls");
                 printf("Senha incorreta");
                 count = 0;
+                voltar();
                 break;
             }else{
                 count++;
-                printf("Usu·rio logado!");
+                printf("Usu√°rio logado!");
                 salvaStruct(userAtual, users, index);
             }
         }
@@ -219,13 +232,13 @@ void cliente(){
     printf("---Interface para clientes---\n");
 }
 
-//INTERFACE DO FUNCION¡RIO
+//INTERFACE DO FUNCION√ÅRIO
 void funcionario(){
     system("cls");
-    printf("---Interface para funcion·rios---\n");
+    printf("---Interface para funcion√°rios---\n");
 }
 
-//ERRO ADCIONADO AO PAR¬METRO
+//ERRO ADCIONADO AO PAR√ÇMETRO
 void error(char erro[]){
     printf("Erro em: %s\n", erro);
 }
@@ -233,7 +246,6 @@ void error(char erro[]){
 //SALVA TODOS STRUCTS PARA MOSTRAR
 void salvaStruct(struct User userAtual, struct User users[20], int indice){
     users[indice] = userAtual;
-    mostraCadastrados(users);
 }
 
 //volta
